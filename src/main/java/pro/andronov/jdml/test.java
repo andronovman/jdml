00992b08d40a0bdc52a80450c436927e4d3fb140
andronovman <andronovman@mail.ru>
2023-04-13 10:49:36 +0400

    setting

--- src/main/java/pro/andronov/jdml/test.java
+++ src/main/java/pro/andronov/jdml/test.java
@@ -1,17 +1,8 @@
 package pro.andronov.jdml;
 
 import org.postgresql.ds.PGSimpleDataSource;
-import pro.andronov.jdml.lib.Column;
-import pro.andronov.jdml.lib.DataSet;
-import pro.andronov.jdml.lib.DataSetFactory;
-import pro.andronov.jdml.lib.Row;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Calendar;
-import java.util.Date;
 
 public class test {
 
@@ -24,22 +15,5 @@ public class test {
         source.setUser("postgres");
         source.setPassword("1");
 
-        DataSetFactory.get(source);
-
-        DataSet paid = DataSetFactory.get().initDataSet("payments", "id", "SELECT nextval('payments_id_seq'::regclass)");
-
-        System.out.println(paid.loadAll());
-
-        /*Calendar cal = Calendar.getInstance();
-
-        for(int i=0; i<10; i++) {
-            cal.add(Calendar.DATE, i);
-            Row r = paid.add();
-            r.setValue("user_id", 79270224207l);
-            r.setValue("pay_date", cal.getTime());
-            r.setValue("amount", Math.random());
-        }
-
-        System.out.println(paid.save().size());*/
     }
 }
